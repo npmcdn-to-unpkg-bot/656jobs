5571201380c72959cf0ef095af61a148d190cb55
payucon5103 <venak5@gmail.com>
2016-03-17 15:54:50 -0600

    created jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Jobs(models.Model):
+	title = models.CharField(max_length=100)
+	company = models.CharField(max_length=50)
+	companyDescription = models.TextField()
+	jobDescription = models.TextField()
+	postedDate = models.DateField(auto_now_add=True)
+	salaryRange = models.CharField(max_length=10)
+	jobImage = models.ImageField(upload_to='company/%Y/%m/%d')
