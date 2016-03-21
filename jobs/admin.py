df937217a627e33a3f06cea3578a516cc6e326d0
payucon5103 <venak5@gmail.com>
2016-03-21 15:59:55 -0600

    termine con el modulo de jobs

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Jobs
+from .models import Jobs, Category
 # Register your models here.
 
 class JobsAdmin(admin.ModelAdmin):
 	prepopulated_fields = { "slug" : ("title",)}
+	list_display = ('title','postedDate','company')
 
 admin.site.register(Jobs,JobsAdmin)
+admin.site.register(Category)
--- jobs/models.py
+++ jobs/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Jobs(models.Model):
+	''' Descripcion  de los empleos '''
 	title = models.CharField(max_length=100, unique=True)
 	company = models.CharField(max_length=50)
 	companyDescription = models.TextField()
@@ -14,6 +15,11 @@ class Jobs(models.Model):
 	salaryRange = models.CharField(max_length=30)
 	slug = models.SlugField(max_length=50, unique=True)
 	jobImage = models.ImageField(upload_to='company/%Y/%m/%d')
+	category = models.ManyToManyField("Category", blank=True)
 
+class Category(models.Model):
+	''' Un empleo puede tener muchas categorias '''
+	name = models.CharField(max_length=50)
+	
 	def __unicode__(self):
-		return self.title
+		return self.name
