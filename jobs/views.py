e30d60dd90ddc124c1bcf4d6bf4d6460aed73d0d
payucon5103 <venak5@gmail.com>
2016-04-14 21:52:54 +0000

    agregue validaciones al register

--- jobs/views.py
+++ jobs/views.py
@@ -6,6 +6,7 @@ from .serializers import JobsSerializer
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login, logout
 from .forms import UserForm
+from utils import username_exists
 #from rest_framework import viewsets
 
 # Create your views here.
@@ -78,20 +79,31 @@ class Register(View):
 		return render(request, "register.html", params)
 	
 	def post(self, request, *args, **kwargs):
-		form = self.register_form(request.POST)
-		if form.is_valid():
-			# cleaned (normalized) data
-			user = form.save(commit=False)
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
-			user.set_password(password)
-			user.save()
-			
-			#returns the User objects if credentials are correct
-			user = authenticate(username=username,password=password)
-			if user is not None and user.is_active:
-				login(request,user)
-				return redirect('/dashboard')
+		if request.POST:
+			form = self.register_form(request.POST)
+			username = request.POST['username']
+			user_exist = username_exists(username)
+			print user_exist
+			if user_exist:
+				error = "Ya existe este usuario elige otro"
+				params = {}
+				params["form"] = form
+				params["error"] = error
+				return render(request,"register.html", params)
+			else:
+				if form.is_valid():
+					# cleaned (normalized) data
+					user = form.save(commit=False)
+					username = form.cleaned_data['username']
+					password = form.cleaned_data['password']
+					user.set_password(password)
+					user.save()
+					
+					#returns the User objects if credentials are correct
+					user = authenticate(username=username,password=password)
+					if user is not None and user.is_active:
+						login(request,user)
+						return redirect('/dashboard')
 		
 class DetailsById(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Jobs.objects.all()
